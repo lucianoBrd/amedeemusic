02982af433e496c7ab8dd7647d6d850ca0d264fd
lucianoBrd <lucien.burdet@gmail.com>
2024-01-27 22:07:54 +0100

    update dep

--- src/app/pages/page-gallery/page-gallery.component.ts
+++ src/app/pages/page-gallery/page-gallery.component.ts
@@ -107,7 +107,7 @@ export class PageGalleryComponent implements OnInit, OnDestroy {
       ),
       ...this.videoGalleries.map(
         item => new VideoItem({ 
-          src: [{url: (this.galleryImagePath + item.image), type: item.mimeType}],
+          //src: [{url: (this.galleryImagePath + item.image), type: item.mimeType}],
           thumb: 'assets/images/music/icons/play.png',
           poster: 'assets/images/music/icons/play.png',
           autoplay: true,
